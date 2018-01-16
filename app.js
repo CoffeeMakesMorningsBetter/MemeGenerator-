f8d4affd13e10bf10dda48ac9f3b2b306c76018b
Mark Suzuki <markdsuzuki@gmail.com>
2018-01-15 19:35:04 -0800

    images with top, bottom text and grid system implemented

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 var meme = document.querySelector('#meme')
 var container = document.querySelector('.container')
+var form = document.querySelector('#form')
 
 meme.addEventListener('click', function(){
 	//TAKE URL CREATE IMAGE AND APPEND TO CONTAINER
 	var url = document.getElementById("url").value 
 	var topline = document.getElementById("topline").value
-	console.log(topline)
+	var bottomline = document.getElementById("bottomline").value
 	var divImg = document.createElement('div')
-	divImg.setAttribute('classs','image')
+	divImg.setAttribute('class','image')
 	var meme = document.createElement('img')
 	var toplinetext = document.createElement('div')
-	// console.log(toplinetext)
+	var bottomlinetext = document.createElement('div')
 	toplinetext.textContent = topline 
-	toplinetext.setAttribute('class', 'top-left')
+	toplinetext.setAttribute('class', 'top-center')
+	bottomlinetext.textContent = bottomline
+	bottomlinetext.setAttribute('class', 'bottom-center')
 	meme.setAttribute('src', url)
 	divImg.appendChild(meme)
 	divImg.appendChild(toplinetext)
+	divImg.appendChild(bottomlinetext)
 	container.appendChild(divImg)
+	//clears fileds in forms 
+	form.reset()
 })
+
+
+
 
